7eff41eeebc7272316a8b0b5800817eef5c6a71e
aaimran236 <ai2015236@gmail.com>
2026-01-21 22:09:42 +0600

    Complete the Fragment

--- app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
+++ app/src/main/java/com/example/juicetracker/EntryDialogFragment.kt
@@ -1,14 +1,24 @@
 package com.example.juicetracker
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
+import com.example.juicetracker.data.JuiceColor
 import com.example.juicetracker.databinding.FragmentEntryDialogBinding
 import com.example.juicetracker.ui.AppViewModelProvider
 import com.example.juicetracker.ui.EntryViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.launch
+
 
 /*
  In Compose, you build layouts declaratively using Kotlin or Java. You can access different
@@ -24,6 +34,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
  */
 
 class EntryDialogFragment : BottomSheetDialogFragment() {
+    var selectedColor = JuiceColor.Red.name
 
     /// onCreateView() function creates the View for this Fragment.
     override fun onCreateView(
@@ -32,10 +43,12 @@ class EntryDialogFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         ///return super.onCreateView(inflater, container, savedInstanceState)
-        return FragmentEntryDialogBinding.inflate(inflater,container,false).root
+        return FragmentEntryDialogBinding.inflate(inflater, container, false).root
     }
 
     private val entryViewModel by viewModels<EntryViewModel> { AppViewModelProvider.Factory }
+
+    private val spinnerColors: List<String> = JuiceColor.entries.map { it.name }
 
     /*
     The onViewCreated() method is called after onCreateView() in the lifecycle. The
@@ -46,7 +59,66 @@ class EntryDialogFragment : BottomSheetDialogFragment() {
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binding = FragmentEntryDialogBinding.bind(view)
-        ///super.onViewCreated(view, savedInstanceState)
+
+        val args: EntryDialogFragmentArgs by navArgs()
+        val juiceId = args.itemId
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_spinner_item,
+            spinnerColors
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.colorSpinner.adapter = adapter
+
+        binding.colorSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectedColor = spinnerColors[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        if (juiceId > 0) {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    entryViewModel.getJuiceStream(juiceId).collect { juice ->
+                        binding.name.setText(juice?.name)
+                        binding.description.setText(juice?.description)
+
+                        val position = spinnerColors.indexOf(juice?.color)
+                        binding.colorSpinner.setSelection(position)
+
+                        binding.ratingBar.rating = juice?.rating?.toFloat() ?: 0F
+                    }
+                }
+            }
+        }
+
+        binding.saveButton.setOnClickListener {
+            entryViewModel.saveJuice(
+                juiceId,
+                binding.name.text.toString(),
+                binding.description.text.toString(),
+                selectedColor,
+                binding.ratingBar.rating.toInt()
+            )
+
+            ///After the data is saved, dismiss the dialog with the dismiss() method.
+            dismiss()
+        }
+
+        binding.cancelButton.setOnClickListener {
+            ///when cancelButton is clicked dismiss the dialog with the dismiss() method
+            dismiss()
+        }
     }
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("com.android.library") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("androidx.navigation.safeargs") version "2.9.3" apply false
+    id("com.android.application") version "9.0.0" apply false
+    id("com.android.library") version "9.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("androidx.navigation.safeargs") version "2.9.6" apply false
 }
